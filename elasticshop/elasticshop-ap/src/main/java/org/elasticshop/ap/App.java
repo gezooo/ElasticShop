611a8ed0299ad7350155f4b911721630580aa9b6
gez <zhangge12131415@gmail.com>
2014-07-05 00:34:57 -0400

    init project

---
+++ elasticshop/elasticshop-ap/src/main/java/org/elasticshop/ap/App.java
@@ -0,0 +1,13 @@
+package org.elasticshop.ap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ elasticshop/elasticshop-mpp/src/main/java/org/gez/elasticshop/mpp/App.java
@@ -0,0 +1,13 @@
+package org.gez.elasticshop.mpp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
